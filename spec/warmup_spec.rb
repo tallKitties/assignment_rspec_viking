4b84f24caf3a16b7ea9c2640c1afb01c265ef192
Ben Kautt <bkautt73@gmail.com>
2017-12-05 14:20:58 -0800

    added #gets_shout spec

--- spec/warmup_spec.rb
+++ spec/warmup_spec.rb
@@ -1,5 +1,13 @@
 require "warmup"
 
 RSpec.describe Warmup do
-  describe
+  let(:warmup){Warmup.new}
+
+  describe '#gets_shout' do
+    it "should shout what it's given" do
+      allow(warmup).to receive(:gets).and_return('test')
+      allow(warmup).to receive(:puts)
+      expect(warmup.gets_shout).to eq('TEST')
+    end
+  end
 end
