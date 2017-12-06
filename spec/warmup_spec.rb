e6ac419ab4e7ae44eea59cddffe64ac882e3732f
Ben Kautt <bkautt73@gmail.com>
2017-12-05 16:47:30 -0800

    finished Warmup specs

--- spec/warmup_spec.rb
+++ spec/warmup_spec.rb
@@ -10,4 +10,41 @@ RSpec.describe Warmup do
       expect(warmup.gets_shout).to eq('TEST')
     end
   end
+
+  describe '#triple_size' do
+    let(:arr){[1,2,3,4]}
+    it "should return the array size * 3" do
+      expect(warmup.triple_size(arr)).to eq(12)
+    end
+  end
+
+  describe '#calls_some_methods' do
+    # let(:string){double('string', "test string")}
+    let(:string){'test string'}
+    let(:up_string){'TEST STRING'}
+    let(:reverse_string){string.reverse}
+    let(:string_id){string.object_id}
+    it "should UPCASE the given string" do
+      expect(string).to receive(:upcase!).and_return(up_string)
+
+      allow(string).to receive(:reverse!).and_return(string)
+
+      warmup.calls_some_methods(string)
+    end
+
+    it "should reverse the given string" do
+      allow(string).to receive(:upcase!).and_return(string)
+
+      expect(string).to receive(:reverse!).and_return(reverse_string)
+
+      warmup.calls_some_methods(string)
+    end
+
+    it "should return a different object" do
+      expect(warmup.calls_some_methods(string).object_id).not_to eq(string_id)
+    end
+    # it "gives object" do
+      # puts string.object_id
+    # end
+  end
 end
