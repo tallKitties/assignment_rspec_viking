18e73e30eeffbdc760cf3bc8e4d4dd497d39e373
Ben Kautt <bkautt73@gmail.com>
2017-12-06 18:20:21 -0800

    added #pick_up_weapon & #drop_weapon specs

--- spec/viking_spec.rb
+++ spec/viking_spec.rb
@@ -1,9 +1,13 @@
 require 'viking'
+require 'weapons/axe'
+require 'weapons/bow'
+require 'weapons/fists'
 
 RSpec.describe Viking do
 
+  let(:viking){Viking.new('Ben')}
+
   describe '#initialize' do
-    let(:viking){Viking.new('Ben')}
 
     it "sets it's name to 'Ben' when I pass that on init" do
       expect(viking.name).to eq('Ben')
@@ -20,6 +24,39 @@ RSpec.describe Viking do
 
     it "starts without a weapon" do
       expect(viking.weapon).to be_nil
+    end
+
+  end
+
+  describe '#pick_up_weapon' do
+    
+    let(:bow){Bow.new}
+    let(:axe){Axe.new}
+   
+    it "should keep the weapon it picks up" do
+      viking.pick_up_weapon(bow)
+      expect(viking.weapon).to eq(bow)
+    end
+
+    it "should not be able to pickup non-weapons" do
+      expect{viking.pick_up_weapon('nope')}.to raise_error(RuntimeError)
+    end
+
+    it "should replace the old weapon when picking up a new one" do
+      [bow, axe].each do |weapon|
+        viking.pick_up_weapon(weapon)
+        expect(viking.weapon).to eq(weapon)
+      end
+    end
+  end
+
+  describe '#drop_weapon' do
+
+    let(:ben){Viking.new('Ben', 100, 10, Bow.new)}
+
+    it "should drop the weapon" do
+      ben.drop_weapon
+      expect(ben.weapon).to be_nil
     end
 
   end
